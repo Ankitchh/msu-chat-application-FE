89229517aaaaa2946eb9b4c45c326ea9b2bdcdbd
Amit4218 <amitbhagat621@gmail.com>
2025-12-06 07:19:32 +0530

    ui:added an loader component to show while the request takes time

---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-white dark:bg-gray-900">
+      <div className="w-12 h-12 border-4 border-transparent border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+}
